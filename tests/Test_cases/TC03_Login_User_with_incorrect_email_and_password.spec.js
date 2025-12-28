5e2ddc8a99a660754ab55dbc00d3d425d667cef6
karankambli25 <kashidakshay989@gmail.com>
2025-12-28 23:05:15 +0530

    feat(auth): add test case for login with incorrect crednetials

---
+++ tests/Test_cases/TC03_Login_User_with_incorrect_email_and_password.spec.js
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("TC03: Login User with incorrect credentials @tc3 @regression", () => {
+    test('Login User with incorrect email and correct password', async({page}) => {
+        const email = "Steve.Irwin.wrong@gmail.com";
+        const password = "Steve@123"
+
+        await page.goto('https://automationexercise.com')
+        await expect(page).toHaveURL('https://automationexercise.com');
+        await expect(page).toHaveTitle('Automation Exercise');
+
+        await page.locator('text=Signup / Login').click();
+        await expect(page).toHaveURL('https://automationexercise.com/login');
+        await expect(page).toHaveTitle('Automation Exercise - Signup / Login');
+
+        await page.locator('[data-qa="login-email"]').click();
+        await page.locator('[data-qa="login-email"]').fill(email);
+
+        await page.locator('[data-qa="login-password"]').click();
+        await page.locator('[data-qa="login-password"]').fill(password);
+
+        await page.locator('[data-qa="login-button"]').click();
+
+        await expect(page.locator('text=Your email or password is incorrect!')).toBeVisible();
+    });
+
+    test('Login User with correct email and incorrect password', async({page}) => {
+        const email = "Steve.Irwin@gmail.com";
+        const password = "Incorrect@123"
+
+        await page.goto('https://automationexercise.com')
+        await expect(page).toHaveURL('https://automationexercise.com');
+        await expect(page).toHaveTitle('Automation Exercise');
+
+        await page.locator('text=Signup / Login').click();
+        await expect(page).toHaveURL('https://automationexercise.com/login');
+        await expect(page).toHaveTitle('Automation Exercise - Signup / Login');
+
+        await page.locator('[data-qa="login-email"]').click();
+        await page.locator('[data-qa="login-email"]').fill(email);
+
+        await page.locator('[data-qa="login-password"]').click();
+        await page.locator('[data-qa="login-password"]').fill(password);
+
+        await page.locator('[data-qa="login-button"]').click();
+
+        await expect(page.locator('text=Your email or password is incorrect!')).toBeVisible();
+    });
+});
