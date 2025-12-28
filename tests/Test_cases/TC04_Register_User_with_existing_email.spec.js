f61be8e1a9ae4083a76c99b690d2a0a203cfa082
karankambli25 <kashidakshay989@gmail.com>
2025-12-29 00:47:00 +0530

    test(auth): add negative test for login with existing email ID

---
+++ tests/Test_cases/TC04_Register_User_with_existing_email.spec.js
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('TC04: Register User with existing email @tc4 @regression', async({page}) => {
+    const firstName = "Steve";
+    const lastName = "Irwin";
+    const email = firstName + "." + lastName + "@gmail.com";
+    const fullName = firstName + " " + lastName;
+
+    await page.goto("https://automationexercise.com");
+    await expect(page).toHaveURL("https://automationexercise.com");
+    await expect(page).toHaveTitle("Automation Exercise");
+
+    await page.locator('text=Signup / Login').click();
+    await expect(page).toHaveURL("https://automationexercise.com/login");
+    await expect(page).toHaveTitle("Automation Exercise - Signup / Login");
+
+    await expect(page.locator('text=New User Signup!')).toBeVisible()
+    await page.locator('[data-qa="signup-name"]').click();
+    await page.locator('[data-qa="signup-name"]').fill(fullName);
+
+    await page.locator('[data-qa="signup-email"]').click();
+    await page.locator('[data-qa="signup-email"]').fill(email);
+
+    await page.locator('[data-qa="signup-button"]').click();
+    await expect(page.locator('text=Email Address already exist!')).toBeVisible()
+})
