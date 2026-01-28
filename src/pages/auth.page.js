3d72bee23bc2f6d6c1495ad4e30cc7a9c40937d2
madhukar2525 <kamblimadhukar2525@gmail.com>
2026-01-28 22:36:29 +0530

    feat: add new functions for auth page

--- src/pages/auth.page.js
+++ src/pages/auth.page.js
@@ -12,7 +12,10 @@ export class AuthPage {
         this.deleteAccountButton = page.getByRole('link', { name: "Delete Account" });
         this.logoutButton = page.getByRole('link', { name: "Logout" });
         this.accountDeletedHeading = page.getByRole('heading', { name: "Account Deleted!" });
-        this.continueButton = page.locator('[data-qa="continue-button"]')
+        this.continueButton = page.locator('[data-qa="continue-button"]');
+        this.loggedInButton = (fullName) => this.page.locator(`text=Logged in as ${fullName}`);
+        this.accountCreatedHeading = this.page.getByRole('heading', { name: "Account Created!" });
+        this.continueButton = this.page.locator('[data-qa="continue-button"]');
     }
 
     async deleteAccount(){
@@ -36,5 +39,23 @@ export class AuthPage {
         await this.deleteAccountButton.click();
         await expect(this.page).toHaveTitle('Automation Exercise');
         await expect(this.page).toHaveURL("https://automationexercise.com");
+    }
+
+    async verifyUserIsLoggedIn(fullName){
+        await expect(this.page).toHaveURL("https://automationexercise.com");
+        await expect(this.page).toHaveTitle("Automation Exercise");
+        await expect(this.loggedInButton(fullName)).toBeVisible();
+        await expect(this.deleteAccountButton).toBeVisible()
+        await expect(this.logoutButton).toBeVisible()
+    }
+
+    async verifyAccountCreatedPageIsLoaded(){
+        await expect(this.page).toHaveTitle('Automation Exercise - Account Created');
+        await expect(this.page).toHaveURL("https://automationexercise.com/account_created");
+        await expect(this.accountCreatedHeading).toBeVisible()
+    }
+
+    async clickContinueButton(){
+        await this.continueButton.click()
     }
 }
