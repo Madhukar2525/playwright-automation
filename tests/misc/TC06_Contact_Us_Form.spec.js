0b08a14348a560747e64a7744d28a52edbca3c60
madhukar2525 <kamblimadhukar2525@gmail.com>
2026-01-31 00:46:07 +0530

    test(contact us): add new test case to verify contact us page

---
+++ tests/misc/TC06_Contact_Us_Form.spec.js
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test';
+import users from '../../src/test-data/users.json' assert { type: 'json'};
+import { HomePage } from '../../src/pages/home.page';
+import { ContactUsPage } from '../../src/pages/contactus.page';
+
+
+test.describe('Misc test cases', { tag: '@regression' }, () => {
+    /** @type {HomePage} */
+    let homepage;
+
+    /** @type {ContactUsPage} */
+    let contactusPage;
+
+    test.beforeEach('Launch website and validate homepage', async({page}) => {
+        homepage = new HomePage(page);
+        contactusPage = new ContactUsPage(page);
+
+        await homepage.launchHomePage();
+        await homepage.verifyHomepage();
+    });
+
+    test('TC05: Contact Us Form', { tag: ['@tc06'] }, async ({ page }) => {
+        const johnUser = users.loginUsers.John;
+        const subject = "Refund is not credited";
+        const message = "I cancelled one order which was prepaid, the parcel has been given to delivery person, but I havent received the refund amount yet.";
+
+        await homepage.clickContactUs();
+        await contactusPage.verifyContactUsPageIsLoaded();
+
+        await contactusPage.fillContactForm(johnUser.fullName, johnUser.emailId, subject, message);
+
+        // await page.setInputFiles('input[type="file"]', 'test-data/dummy_file.pdf');
+        await page.locator('input[type="file"]').setInputFiles('src/test-data/dummy_file.pdf')
+
+        page.once('dialog', async dialog => {
+            console.log(`Dialog type: ${dialog.type()}`); // "confirm"
+            console.log(`Dialog message: ${dialog.message()}`); // "Press OK to proceed"
+            await dialog.accept(); // clicks OK
+            // If you wanted Cancel: await dialog.dismiss();
+        });
+
+
+        await page.locator('[data-qa="submit-button"]').click();
+
+        await contactusPage.verifySuccessMessage()
+
+        await contactusPage.clickHome();
+        await homepage.verifyHomepage();
+    });
+
+});
