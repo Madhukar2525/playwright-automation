9cd8c5a4d265f6e3b81587b3a0436c41adb170d4
madhukar2525 <kamblimadhukar2525@gmail.com>
2026-02-04 00:55:54 +0530

    refactor: update script to use POM

--- tests/cart/TC13_Verify_Product_quantity_in_Cart.spec.js
+++ tests/cart/TC13_Verify_Product_quantity_in_Cart.spec.js
@@ -1,51 +1,58 @@
 import { expect, test } from "@playwright/test";
+import { HomePage } from "../../src/pages/home.page";
+import { ProductPage } from "../../src/pages/product.page";
+import { CartPage } from "../../src/pages/cart.page";
 
-test.fixme('TC13: Verify Product quantity in Cart @tc13 @regression', async({ page }) => {
-    await page.goto("https://automationexercise.com", { waitUntil: 'domcontentloaded' });
-    await expect(page).toHaveURL("https://automationexercise.com");
-    await expect(page).toHaveTitle("Automation Exercise");
+test.describe('Cart related test cases', { tag: ['@regression', '@cart'] }, () => {
+    /** @type {HomePage} */
+    let homePage;
 
-    // -- Go to Products section
-    await page.getByRole('link', { name: " Products"}).click();
-    await expect(page).toHaveURL("https://automationexercise.com/products");
-    await expect(page).toHaveTitle("Automation Exercise - All Products");
-    await expect(page.getByRole('heading', { name: "All Products"})).toBeVisible();
-    await expect(page.locator('[class="features_items"]')).toBeVisible()
+    /** @type {ProductPage} */
+    let productPage;
 
-    const productsList = page.locator('[class="product-image-wrapper"]');
-    const productsCount = await productsList.count();
-    expect(productsCount).toBeGreaterThan(0);
+    /** @type {CartPage} */
+    let cartPage;
 
-    const productNum = 1;
-    const firstProduct = productsList.nth(productNum-1);
-    await firstProduct.getByRole('link', { name: "View Product"}).click();
-    await expect(page).toHaveURL(`https://automationexercise.com/product_details/${productNum}`);
-    await expect(page).toHaveTitle("Automation Exercise - Product Details");
-    await expect(page.locator('[class="product-information"]')).toBeVisible();
 
-    // -- Increase quantity to 4
-    const quantity = '4';
-    const quantityBox = page.locator('[id="quantity"]');
-    await quantityBox.clear();
-    await quantityBox.fill(quantity);
+    test.beforeEach('Launch website and verify homepage', async({ page }) => {
+        homePage = new HomePage(page);
+        productPage = new ProductPage(page);
+        cartPage = new CartPage(page);
 
-    // -- Click Add to cart button
-    await page.getByRole('button', { name: "Add to cart"}).click();
-    await expect(page.locator('[class="modal-content"]')).toBeVisible();
-    await expect(page.locator('[class="text-center"]', { hasText: "Your product has been added to cart."})).toBeVisible();
+        await homePage.launchHomePage();
+        await homePage.verifyHomepageIsLoaded();
+    });
 
-    // -- Click View Cart button
-    await page.locator('a', { hasText: "View Cart"}).click()
-    await expect(page).toHaveURL('https://automationexercise.com/view_cart');
-    await expect(page).toHaveTitle("Automation Exercise - Checkout");
+    test('TC13: Verify Product quantity in Cart', { tag: ['@tc13'] }, async ({ page }) => {
 
-    const cartInfo = page.locator('[id="cart_info"]');
-    await expect(cartInfo).toBeVisible();
-    const itemsRow = cartInfo.locator('tbody tr');
-    const itemsCount = await itemsRow.count()
-    expect(itemsCount).toBeGreaterThan(0);
+        // -- Go to Products section
+        await homePage.clickProduct();
+        await productPage.verifyProductPageIsLoaded();
 
-    const itemQuantity = await itemsRow.nth(0).locator('[class="cart_quantity"]').innerText();
-    expect(itemQuantity).toBe(quantity);
+        const count = await productPage.getProductsCount();
+        expect(count).toBeGreaterThan(0);
 
-})
+        const productNum = 1;
+        await productPage.clickViewProductByIndex(productNum);
+        await productPage.verifyProductDetailsPageIsLoaded(productNum);
+
+        // -- Increase quantity to 4
+        const productQuantity = 4
+        await productPage.setProductQuantity(productQuantity);
+
+        // -- Click Add to cart button
+        await productPage.clickAddToCart();
+        await productPage.verifyAddToCartConfirmation();
+
+        // -- Click View Cart button
+        await productPage.clickViewCart();
+        await cartPage.verifyCartPageIsLoaded();
+
+        const cartItemsCount = await cartPage.getCartProductCount();
+        expect(cartItemsCount).toBeGreaterThan(0);
+
+        await cartPage.verifyCartItemDetailsByIndex(1, { price: 500, quantity: productQuantity });
+
+    });
+
+});
