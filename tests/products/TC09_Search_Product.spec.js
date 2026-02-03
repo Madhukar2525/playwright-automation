c6cbd0e4daded4b299487c7c265842a00fbd6166
madhukar2525 <kamblimadhukar2525@gmail.com>
2026-02-03 13:48:08 +0530

    refactor(search): refactor test case to use POM

--- tests/products/TC09_Search_Product.spec.js
+++ tests/products/TC09_Search_Product.spec.js
@@ -1,37 +1,50 @@
 import { expect, test } from "@playwright/test";
+import { HomePage } from "../../src/pages/home.page";
+import { ProductPage } from "../../src/pages/product.page";
 
-test.fixme('TC09: Search Product @tc9 @regression', async({ page }) => {
-    await page.goto("https://automationexercise.com", { waitUntil: 'domcontentloaded' });
-    await expect(page).toHaveURL("https://automationexercise.com");
-    await expect(page).toHaveTitle("Automation Exercise");
+test.describe('Search Product', { tag: ['@regression'] }, () => {
+    /** @type {HomePage} */
+    let homePage;
 
-    await page.getByRole('link', { name: 'Products'}).click()
+    /** @type {ProductPage} */
+    let productPage;
 
-    await expect(page).toHaveURL("https://automationexercise.com/products");
-    await expect(page).toHaveTitle("Automation Exercise - All Products");
-    await expect(page.getByRole('heading',{ name: 'All Products'})).toBeVisible();
-    await expect(page.locator('[class="features_items"]')).toBeVisible()
+    test.beforeEach('Launch the website and validate homepage', async ({ page }) => {
+        homePage = new HomePage(page);
+        productPage = new ProductPage(page);
 
-    const productsList = page.locator('[class="productinfo text-center"]');
-    const productsCount = await productsList.count();
-    console.log("total products count is: ", productsCount)
-    expect(productsCount).toBeGreaterThan(0);
+        await homePage.launchHomePage();
+        await homePage.verifyHomepage();
+    });
 
-    const searchText = "blue".toLowerCase()
-    await page.locator('[id="search_product"]').fill(searchText);
-    await page.locator('[id="submit_search"]').click();
+    test('TC09: Search Product', { tag: ['@tc09']}, async ({ page }) => {
+        await homePage.clickProduct();
+        await productPage.verifyProductPageIsLoaded();
 
-    await expect(page).toHaveURL(`https://automationexercise.com/products?search=${searchText}`);
-    await expect(page.getByRole('heading', { name: "Searched Products"})).toBeVisible();
+        const productsCount = await productPage.getProductsCount();
+        expect(productsCount).toBeGreaterThan(0);
 
-    const searchedProductsList = page.locator('[class="productinfo text-center"]');
-    const sPCount = await searchedProductsList.count();
-    console.log("total products count is: ", sPCount)
-    expect(sPCount).toBeGreaterThan(0);
+        await productPage.searchForProduct("blue")
 
-    for (let i=0; i<sPCount; i++){
-        const productName = (await searchedProductsList.nth(i).locator('p').innerText()).toLowerCase();
-        console.log(`Product name of ${i+1} is: `, productName);
-        expect.soft(productName).toContain(searchText)
-    }
-})
+        // await expect(page).toHaveURL(`https://automationexercise.com/products?search=${searchText}`);
+        // await expect(page.getByRole('heading', { name: "Searched Products" })).toBeVisible();
+        await productPage.verifySearchResultPage();
+
+        const spCount = await productPage.getSearchedProductsCount();
+        expect(spCount).toBeGreaterThan(0);
+
+        // const searchedProductsList = page.locator('[class="productinfo text-center"]');
+        // const sPCount = await searchedProductsList.count();
+        // console.log("total products count is: ", sPCount)
+        // expect(sPCount).toBeGreaterThan(0);
+
+        // for (let i = 0; i < sPCount; i++) {
+        //     const productName = (await searchedProductsList.nth(i).locator('p').innerText()).toLowerCase();
+        //     console.log(`Product name of ${i + 1} is: `, productName);
+        //     expect.soft(productName).toContain(searchText)
+        // }
+        await productPage.verifyAllProductsContainsSearchText();
+
+    });
+
+});
